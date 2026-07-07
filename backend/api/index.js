dd525c89f6caf0b0032056c0e595236fb84b5bba
Rahul Kumar <rahulkumar@Rahuls-MacBook-Air.local>
2026-07-07 15:44:00 +0530

    Test Express app without DB initialization

--- backend/api/index.js
+++ backend/api/index.js
@@ -4,25 +4,4 @@ const serverless = require("serverless-http");
 
 const app = require("../src/app");
 
-const { connect } = require("../src/utils/db");
-const redisClient = require("../src/utils/redisClient");
-
-let initialized = false;
-
-async function initialize() {
-  if (initialized) return;
-
-  await connect();
-
-  if (!redisClient.isOpen) {
-    await redisClient.connect();
-  }
-
-  initialized = true;
-}
-
-module.exports = async (req, res) => {
-  await initialize();
-
-  return serverless(app)(req, res);
-};
+module.exports = serverless(app);
